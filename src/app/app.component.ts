ce11b3803b6cdaf996b574af5535fc4ba8405162
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-10-28 09:25:20 -0300

    Added parentheses for subscribe inside create bird, changed curly brackets, added commas and indentations
    build was not compiling and was giving an error inside app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,45 +20,45 @@ export class AppComponent implements OnInit {
   public deleteBird!: Bird;
 
   constructor(private birdService: BirdService){}
-    ngOnInit(): void {
-      this.getBird(); 
-    } 
+  ngOnInit(): void {
+    this.getBird();
+  }
 
-    public getBird(): void {
-      this.birdService.getBird()
+  public getBird(): void {
+    this.birdService.getBird()
     .subscribe(_bird => {
       this.bird.push();
       console.log(this.bird);
-    });
-    
+    },
     (error: HttpErrorResponse) => {
       alert(error.message);
     }
-    }
+    );
+  }
 
-    public createBird(addForm: NgForm): void {
-      document.getElementById('create-bird-form')?.click()
+  public createBird(addForm: NgForm): void {
+    document.getElementById('create-bird-form')?.click()
     this.birdService.addBird(addForm.value).subscribe(
       (response: Bird) => {
         console.log(response);
         this.getBird();
         addForm.reset();
-        
-      (error: HttpErrorResponse) => {
-      alert(error.message);
-    }
-    }
-  }
-
-  public onUpdateBird(bird: Bird): void {
-    this.birdService.updateBird(bird!).subscribe(
-      (response: Bird) => {
-        console.log(response);
-        this.getBird();
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
-    );
-  }
-}
+      );
+    }
+
+    public onUpdateBird(bird: Bird): void {
+      this.birdService.updateBird(bird!).subscribe(
+        (response: Bird) => {
+          console.log(response);
+          this.getBird();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+        );
+      }
+    }
